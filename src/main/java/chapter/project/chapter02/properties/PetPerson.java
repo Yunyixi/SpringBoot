572c55a03edaedaeaa1dffada7a8e8ac7d7bcf1c
FeiLong <207857259@qq.com>
2023-09-07 15:10:03 +0800

    优化chapter02，完善相关结构。

---
+++ src/main/java/chapter/project/chapter02/properties/PetPerson.java
@@ -0,0 +1,85 @@
+package chapter.project.chapter02.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author IDEA
+ * @Date 2023/9/7
+ */
+@Component
+//@ConfigurationProperties(prefix = "petperson") //通过全局自定义文件赋值
+@ConfigurationProperties(prefix = "petpersonyaml") //通过yaml文件赋值
+public class PetPerson {
+    private int id;
+    private String name;
+    private List hobby;
+    private String[] family;
+    private Map map;
+    private Pet pet;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(List hobby) {
+        this.hobby = hobby;
+    }
+
+    public String[] getFamily() {
+        return family;
+    }
+
+    public void setFamily(String[] family) {
+        this.family = family;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    @Override
+    public String toString() {
+        return "PetPerson{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", hobby=" + hobby +
+                ", family=" + Arrays.toString(family) +
+                ", map=" + map +
+                ", pet=" + pet +
+                '}';
+    }
+}
--- src/test/java/chapter/project/chapterApplicationTest.java
+++ src/test/java/chapter/project/chapterApplicationTest.java
@@ -1,8 +1,8 @@
 package chapter.project;
 
 import chapter.project.chapter01.ChapterController;
+import chapter.project.chapter02.properties.PetPerson;
 import chapter.project.chapter03.batis.BatisCommentMapper;
-import chapter.project.chapter03.jpa.JpaDiscuss;
 import chapter.project.chapter03.jpa.JpaDiscussRepository;
 import chapter.project.chapter03.redis.RedisPersonRepository;
 import org.junit.Test;
@@ -10,8 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 /**
  * @Description
@@ -29,7 +27,8 @@ public class chapterApplicationTest {
     private ChapterController chapterController; //注入ChapterController类
 
     //chapter02
-
+    @Autowired
+    private PetPerson petPerson;
 
     //chapter03
     @Autowired
@@ -47,31 +46,38 @@ public class chapterApplicationTest {
     }
 
     @Test
-    public void Chapter03() {
-        //查询MySQL数据信息操作
-        //System.out.println(commentMapper.findById(1));
-        //System.out.println(commentMapper.findById(4));
-        //Comment comment = new Comment();
+    public void Chapter02() {
+        System.out.println("properties01单元测试返回的数据：" + petPerson);
 
-        //更新MySQL数据信息操作
-        //comment.setId(7);
-        //comment.setContent("真好，点个赞");
-        //comment.setAuthor("小苏");
-        //commentMapper.updateComment(comment);
+    }
 
 
-        //插入MySQL数据信息操作
-        //comment.setContent("很好，挺不错的");
-        //comment.setAuthor("小发");
-        //comment.setaId(3);
-        //commentMapper.insertComment(comment);
+    //@Test //chapter01测试方法
+    //public void Chapter03() {
+    //查询MySQL数据信息操作
+    //System.out.println(commentMapper.findById(1));
+    //System.out.println(commentMapper.findById(4));
+    //Comment comment = new Comment();
 
-        //删除MySQL数据信息操作
-        //commentMapper.deleteComment(8);
+    //更新MySQL数据信息操作
+    //comment.setId(7);
+    //comment.setContent("真好，点个赞");
+    //comment.setAuthor("小苏");
+    //commentMapper.updateComment(comment);
 
-        //jpa实现，实现数据操作
-        List<JpaDiscuss> jpalist = jpaDiscussRepository.findByAuthorNotNull();
-        System.out.println(jpalist);
+
+    //插入MySQL数据信息操作
+    //comment.setContent("很好，挺不错的");
+    //comment.setAuthor("小发");
+    //comment.setaId(3);
+    //commentMapper.insertComment(comment);
+
+    //删除MySQL数据信息操作
+    //commentMapper.deleteComment(8);
+
+    //jpa实现，实现数据操作
+    //List<JpaDiscuss> jpalist = jpaDiscussRepository.findByAuthorNotNull();
+    //System.out.println(jpalist);
 
         /*//redis实现，实现数据库操作
         RedisPerson redisPerson = new RedisPerson("张", "有才");
@@ -91,5 +97,5 @@ public class chapterApplicationTest {
         RedisPerson save2 = redisPersonRepository.save(redisPerson2);
         System.out.println(save);
         System.out.println(save2);*/
-    }
+    //}
 }
