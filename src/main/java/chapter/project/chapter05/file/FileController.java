4ba14c29f0d1acc9ff7e06a9f87f6dfccf5e4098
FeiLong <207857259@qq.com>
2023-09-19 10:38:57 +0800

    5.3 Spring Boot 文件下载功能拓展实现，并完善相关注释

--- src/main/java/chapter/project/chapter05/file/FileController.java
+++ src/main/java/chapter/project/chapter05/file/FileController.java
@@ -1,5 +1,10 @@
 package chapter.project.chapter05.file;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +19,15 @@ import java.util.UUID;
  * @注释 文件控制管理类
  */
 @Controller
-public class FileController { //http://localhost:8084/toUpload
-
-    //向文件上传页面跳转，页面映射
-    @GetMapping("/toUpload")
+public class FileController {
+    // 5.1文件上传
+    // 向文件上传页面跳转，页面映射
+    @GetMapping("/toUpload") //http://localhost:8084/toUpload
     public String toUpload() {
         return "upload";
     }
 
-    //文件上传
+    // 文件上传
     @PostMapping("/uploadFile")
     public String uploadFile(MultipartFile[] fileUpload, Model model) {
         // 默认文件上传成功，并返回信息状态
@@ -49,5 +54,36 @@ public class FileController { //http://localhost:8084/toUpload
         // 携带上传状态信息回调到文件上传页面
         return "upload";
     }
+
+    // 5.2文件下载
+    // 向文件下载页面跳转，页面映射
+    @GetMapping("/toDownload") //http://localhost:8084/toDownload
+    public String toDownload() {
+        return "download";
+    }
+
+    // 文件下载管理
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> fileDownload(String filename) {
+        // 指定要下载的文件根路径
+        String dirPath = "D:/Recording/SpringBootData/";
+        // 创建该文件的对象
+        File file = new File(dirPath + File.separator + filename);
+        // 设计响应开头
+        HttpHeaders headers = new HttpHeaders();
+        // 通知浏览器以下载的方式打开
+        headers.setContentDispositionFormData("attachment", filename);
+        // 定义以流的方式下载返回文件数据
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        try {
+            return new ResponseEntity<>(FileUtils.readFileToByteArray(file),
+                    headers, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<byte[]>(e.getMessage().getBytes(),
+                    HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
 
 }
