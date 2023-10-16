a0652cae8d98e90eca911b1f77988890c5543ebf
FeiLong <207857259@qq.com>
2023-10-16 23:39:01 +0800

    自定义用户认证 - JDBC身份认证

--- src/main/java/chapter/project/chapter07/SecurityConfig.java
+++ src/main/java/chapter/project/chapter07/SecurityConfig.java
@@ -1,12 +1,10 @@
 package chapter.project.chapter07;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 
 import javax.sql.DataSource;
 
@@ -24,11 +22,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //  密码需要设置编码器
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         // 1、使用内存用户信息，作为测试使用
-        auth.inMemoryAuthentication().passwordEncoder(encoder)
+        /*auth.inMemoryAuthentication().passwordEncoder(encoder)
                 .withUser("shitou").password(encoder.encode("123456")).roles("common")
                 .and()
                 .withUser("李四").password(encoder.encode("123456")).roles("vip");
+        */
+        // 2、使用 JDBC 进行身份认证,数据库
+        String userSQL = "select username,password,valid from t_customer " + "where username = ?";
+        String authoritySQL = "select c.username,a.authority from t_customer c, " + "t_authority 	a,t_customer_authority ca where " + "ca.customer_id=c.id and ca.authority_id=a.id and 	c.username =?";
+        auth.jdbcAuthentication().passwordEncoder(encoder)
+                .dataSource(dataSource)
+                .usersByUsernameQuery(userSQL)
+                .authoritiesByUsernameQuery(authoritySQL);
+
     }
 
 }
